5a08c71f7abe08acd1eaf55cb21bea98e548eb45
spacedvoid <114347266+spacedvoid@users.noreply.github.com>
2026-02-24 18:35:40 +0900

    Add tutorial for the annotation

--- annotations/src/main/kotlin/ConstructorLike.kt
+++ annotations/src/main/kotlin/ConstructorLike.kt
@@ -1,5 +1,5 @@
 /*
- * Copyright 2025 spacedvoid
+ * Copyright 2025-2026 spacedvoid
  *
  * This Source Code Form is subject to the terms of the Mozilla Public
  * License, v. 2.0. If a copy of the MPL was not distributed with this
@@ -11,4 +11,10 @@ package io.github.spacedvoid.constructorlike
 @Retention(AnnotationRetention.SOURCE)
 @Target(AnnotationTarget.FUNCTION)
 @MustBeDocumented
+/**
+ * Marks this function as a pseudo-constructor for Dokka.
+ * Has no effect on source code.
+ *
+ * For more information, refer to this project on [github](https://github.com/spacedvoid/constructor-like).
+ */
 annotation class ConstructorLike
---
+++ resources/markdown/tutorial.md
@@ -0,0 +1,12 @@
+# <code style="color:#e7bf6a">@ConstructorLike</code>
+
+This annotation probably means nothing: just use the constructor.
+If your compiler complains that such constructor doesn't exist, add an import to a symbol named `invoke`
+in the same package as the class(ex. `com.example.MyClass` → `import com.example.invoke`).
+
+---
+
+If you really want to know what the annotation does, click the link left to the constructor.
+The function you'll see is the *constructor* you were using.
+
+For more details, feel free to explore this project.
